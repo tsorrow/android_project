ac48acf08050a2f41ddbc1ce7f5219719e4414cc
tsorrow <1014325951@qq.com>
2019-01-08 18:13:42 +0800

    add network

---
+++ junior/network/src/main/java/com/tsorrow/junior/network/MainActivity.java
@@ -0,0 +1,46 @@
+package com.tsorrow.junior.network;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView responseText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.send_request){
+            sendRequestWithHttpURLConnection();
+        }
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try{
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
