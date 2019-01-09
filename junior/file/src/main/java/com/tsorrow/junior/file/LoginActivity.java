35ba5b6881a9f76625c81569c6382b4c7a9b3ce5
tsorrow <1014325951@qq.com>
2019-01-10 00:24:25 +0800

    add file_3

--- junior/file/src/main/java/com/tsorrow/junior/file/LoginActivity.java
+++ junior/file/src/main/java/com/tsorrow/junior/file/LoginActivity.java
@@ -1,13 +1,91 @@
 package com.tsorrow.junior.file;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private MyDatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        dbHelper = new MyDatabaseHelper(this,"BookStore.db",null,1);
+
+        Button createDatabase = (Button) findViewById(R.id.create);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button insert = (Button) findViewById(R.id.insert);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put("name","aaa");
+                values.put("author","zhangsan");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book",null,values);
+                Log.d(TAG, "insert succeeded");
+                values.put("name","bbb");
+                values.put("author","lisi");
+                values.put("pages",510);
+                values.put("price",19.95);
+                db.insert("Book",null,values);
+            }
+        });
+
+        Button update = (Button) findViewById(R.id.update);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put("price",10.99);
+                db.update("Book",values,"name = ?",new String[]{"aaa"});
+            }
+        });
+
+        Button delete = (Button) findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //db.delete("Book","pages > ?",new String[]{"500"});
+                db.execSQL("delete from Book where pages > ?",new String[]{"500"});
+            }
+        });
+
+        Button query = (Button) findViewById(R.id.query);
+        query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    do{
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        String pages = cursor.getString(cursor.getColumnIndex("pages"));
+                        String price = cursor.getString(cursor.getColumnIndex("price"));
+
+                        String s = name + author + pages + price;
+                        Log.d(TAG, s);
+
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
     }
 }
